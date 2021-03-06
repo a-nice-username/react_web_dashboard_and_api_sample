d8721207c46a3e51b91970f92de1d12fb74c2939
a-nice-username <rynvva@gmail.com>
2021-03-06 13:11:45 +0700

    improve dashboard layout

--- dashboard/src/routes/home.tsx
+++ dashboard/src/routes/home.tsx
@@ -1,47 +1,81 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function Home() {
+  const [ selectedSection, setSelectedSection ] = useState<'dashboard' | 'accounts' | 'pictures' | 'registerAnAccount'>('dashboard')
+
   useEffect(() => {
     handleIfNotAlreadyLoggedIn()
   }, [])
   
   return (
     <div
-      className = 'container'
+      className = 'dashboard_container'
     >
-      <h2
-        style = {{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          marginLeft: 5,
-          marginRight: 5,
-          textAlign: 'center'
-        }}
+      <div
+        className = 'dashboard_side_bar'
       >
-        Pictures App
-        
-        <div
-          style = {{
-            color: 'deepskyblue',
-            marginLeft: 5,
-            marginRight: 5
-          }}
+        <h2
+          className = 'dashboard_side_bar_title'
         >
-          {'| Admin'}
-        </div>
-      </h2>
+          Pictures App
+        </h2>
 
-      <a
-        href = 'javascript:void(0)'
-        onClick = {logout}
-        style = {{
-          marginTop: 20,
-          textDecorationLine: 'none'
-        }}
-      >
-        Logout
-      </a>
+        <div
+          className = 'dashboard_side_bar_content'
+        >
+          <div
+            className = 'dashboard_section'
+          >
+            MAIN NAVIGATION
+          </div>
+
+          <a
+            className = {selectedSection == 'dashboard' ? 'dashboard_section_selected_item' : 'dashboard_section_item'}
+            href = 'javascript:void(0)'
+            onClick = {() => setSelectedSection('dashboard')}
+          >
+            Dashboard
+          </a>
+
+          <a
+            className = {selectedSection == 'accounts' ? 'dashboard_section_selected_item' : 'dashboard_section_item'}
+            href = 'javascript:void(0)'
+            onClick = {() => setSelectedSection('accounts')}
+          >
+            Accounts
+          </a>
+
+          <a
+            className = {selectedSection == 'pictures' ? 'dashboard_section_selected_item' : 'dashboard_section_item'}
+            href = 'javascript:void(0)'
+            onClick = {() => setSelectedSection('pictures')}
+          >
+            Pictures
+          </a>
+
+          <div
+            className = 'dashboard_section'
+          >
+            OTHER
+          </div>
+
+          <a
+            className = {selectedSection == 'registerAnAccount' ? 'dashboard_section_selected_item' : 'dashboard_section_item'}
+            href = 'javascript:void(0)'
+            onClick = {() => setSelectedSection('registerAnAccount')}
+          >
+            Register An Account
+          </a>
+
+          <a
+            className = 'dashboard_section_item'
+            href = 'javascript:void(0)'
+            onClick = {logout}
+          >
+            Logout
+          </a>
+        </div>
+      </div>
     </div>
   )
 
@@ -63,3 +97,37 @@ function Home() {
 }
 
 export default Home
+
+{/* <h2
+  style = {{
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: 'center'
+  }}
+>
+  Pictures App
+  
+  <div
+    style = {{
+      color: 'deepskyblue',
+      marginLeft: 5,
+      marginRight: 5
+    }}
+  >
+    {'| Admin'}
+  </div>
+</h2>
+
+<a
+  href = 'javascript:void(0)'
+  onClick = {logout}
+  style = {{
+    marginTop: 20,
+    textDecorationLine: 'none'
+  }}
+>
+  Logout
+</a> */}
